5c57e42cfdbd36fa470b6cbb29a8c66bac7ee89c
Uchchas Das Gupta <dasgupta.uchchas@gmail.com>
2020-03-28 17:37:07 +0530

    all method removed and add message method

--- Hello.java
+++ Hello.java
@@ -1,11 +1,10 @@
 public class Hello {
     public static void main(String[]args){
         System.out.println("Vs Code is Awesome...");
-    
-    display();
     }
+    public static void message(){
+        System.out.println("Hooray! Now we are able to contribute on open source...");
 
-    public static void display(){
-        System.out.println("Hello Hello");
     }
+   
 }
