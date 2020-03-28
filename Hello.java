42d7ac9790f34e1926970383637d8e50dc6740a1
Uchchas Das Gupta <dasgupta.uchchas@gmail.com>
2020-03-28 16:43:42 +0530

    Added a new method called display to Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("Vs Code is Awesome...");
+    
+    greeting("Uchchas ");
+    }
+
+    public static void greeting(String name){
+        System.out.println("Welcome " + name);
+    }
+}
