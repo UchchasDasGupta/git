b6ce32fa8f9e3c6fda98a3caa403bdadf7d69cd7
Uchchas Das Gupta <dasgupta.uchchas@gmail.com>
2020-04-06 17:03:44 +0530

    delete previous method and add new method

--- Hello.java
+++ Hello.java
@@ -2,19 +2,7 @@ public class Hello {
     public static void main(String[]args){
         System.out.println("Vs Code is Awesome...");
     }
-    public static void message(){
-        System.out.println("Hooray! Now we are able to contribute on open source...");
-
-    }
-<<<<<<< HEAD
-    public static int sqr(int x){
-        return x*x;
-    }
-=======
-   
->>>>>>> anotherBranch
-
-public static void add(int a, int b){
-    return a+b;
-}
+  public static void sqr(int x){
+      return x*x;
+  }
 }
